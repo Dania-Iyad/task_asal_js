120d847984c61f8c2d7ccb2fbece2b2d2657ae1b
Dania Iyad <dania111iyad@gmail.com>
2025-12-27 20:39:54 +0200

    Initial commit

---
+++ script.js
@@ -0,0 +1,165 @@
+document.addEventListener("DOMContentLoaded", () => {
+
+    var todoForm = document.getElementById("todoForm");
+    var todoInput = document.getElementById("todo-text");
+    var errorMessage = document.getElementById("errorMessage");
+    var todoItems = document.getElementById("todoItems");
+
+    var renamePopup = document.getElementById("rename-message");
+    var renameInput = document.getElementById("renameInput");
+    var saveRenameBtn = document.getElementById("saveRename");
+    var cancelRenameBtn = document.getElementById("cancelRename");
+
+    var deletePopup = document.getElementById("icon-message");
+    var confirmDeleteBtn = document.getElementById("confirmDelete");
+    var cancelDeleteBtn = document.getElementById("cancelDelete");
+
+    var deleteDoneTasks = document.getElementById("deleteDoneTasks");
+    var deleteAllTasks = document.getElementById("deleteAllTasks");
+
+    var todos = JSON.parse(localStorage.getItem("todos")) || [];
+    var indexToRename = null;
+    var indexToDelete = null;
+    var currentFilter = "all";
+
+    function saveTodos() {
+        localStorage.setItem("todos", JSON.stringify(todos));
+    }
+
+    function showTodos() {
+        todoItems.innerHTML = "";
+
+        todos.forEach((todo, index) => {
+
+            if (currentFilter === "done" && !todo.done) return;
+            if (currentFilter === "todo" && todo.done) return;
+
+            var li = document.createElement("li");
+            li.className = "todo-item";
+
+            var span = document.createElement("span");
+            span.textContent = todo.text;
+            span.style.textDecoration = todo.done ? "line-through" : "none";
+            span.style.color = todo.done ? "red" : "black";
+
+            var actions = document.createElement("div");
+
+            var checkbox = document.createElement("input");
+checkbox.type = "checkbox";
+checkbox.checked = todo.done;
+
+if (todo.done) {
+    checkbox.classList.add("checked");
+}
+
+checkbox.onclick = () => {
+    todo.done = !todo.done;
+
+    if (todo.done) {
+        checkbox.classList.add("checked");
+    } else {
+        checkbox.classList.remove("checked");
+    }
+
+    saveTodos();
+    showTodos();
+};
+
+
+            var editIcon = document.createElement("i");
+            editIcon.className = "fa-solid fa-pen";
+            editIcon.onclick = () => {
+                renamePopup.style.display = "flex";
+                renameInput.value = todo.text;
+                indexToRename = index;
+            };
+
+            var deleteIcon = document.createElement("i");
+            deleteIcon.className = "fa-solid fa-trash";
+            deleteIcon.onclick = () => {
+                deletePopup.style.display = "flex";
+                indexToDelete = index;
+            };
+
+            actions.append(checkbox, editIcon, deleteIcon);
+            li.append(span, actions);
+            todoItems.appendChild(li);
+        });
+    }
+
+    todoForm.onsubmit = e => {
+        e.preventDefault();
+        var text = todoInput.value.trim();
+
+        if (text === "") {
+            errorMessage.innerText = "Task cannot be empty";
+            errorMessage.classList.add("show");
+            return;
+        }
+
+        if (text[0] >= '0' && text[0] <= '9') {
+            errorMessage.innerText = "Task cannot start with number";
+            errorMessage.classList.add("show");
+            return;
+        }
+
+        if (text.length < 5) {
+            errorMessage.innerText = "Task must be at least 5 characters long";
+            errorMessage.classList.add("show");
+            return;
+        }
+
+        errorMessage.textContent = "";
+        todos.push({ text, done: false });
+        saveTodos();
+        showTodos();
+        todoInput.value = "";
+    };
+
+    saveRenameBtn.onclick = () => {
+        todos[indexToRename].text = renameInput.value;
+        saveTodos();
+        showTodos();
+        renamePopup.style.display = "none";
+    };
+
+    cancelRenameBtn.onclick = () => renamePopup.style.display = "none";
+
+    confirmDeleteBtn.onclick = () => {
+        todos.splice(indexToDelete, 1);
+        saveTodos();
+        showTodos();
+        deletePopup.style.display = "none";
+    };
+
+    cancelDeleteBtn.onclick = () => deletePopup.style.display = "none";
+
+    deleteDoneTasks.onclick = () => {
+        todos = todos.filter(t => !t.done);
+        saveTodos();
+        showTodos();
+    };
+
+    deleteAllTasks.onclick = () => {
+        todos = [];
+        saveTodos();
+        showTodos();
+    };
+
+    document.querySelector('[data-filter="all"]').onclick = () => {
+        currentFilter = "all";
+        showTodos();
+    };
+
+    document.querySelector('[data-filter="done"]').onclick = () => {
+        currentFilter = "done";
+        showTodos();
+    };
+
+    document.querySelector('[data-filter="todo"]').onclick = () => {
+        currentFilter = "todo";
+        showTodos();
+    };
+
+    showTodos();
+});
